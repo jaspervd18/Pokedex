43479b224f9ac0764fe69192bd601267f54dccb3
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-11-28 18:34:11 +0100

    implemented navigation

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import androidx.core.text.HtmlCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        var navView : NavigationView = findViewById(R.id.navView)
+        //or use binding!
+
+        val navController = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
+
+        val titleString = "<font color=\"black\">" + getString(R.string.app_name) + "</font>"
+        supportActionBar?.setTitle(HtmlCompat.fromHtml(titleString, HtmlCompat.FROM_HTML_MODE_LEGACY))
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
 }
--- app/src/main/java/com/example/pokedex/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/pokedex/screens/home/HomeFragment.kt
@@ -1,20 +1,94 @@
 package com.example.pokedex.screens.home
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.pokedex.R
+import com.example.pokedex.databinding.FragmentHomeBinding
 
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class HomeFragment : Fragment() {
+
+    /*
+    Note: the HomeFragment doesn't follow the MVVM architecture to demonstrate the alternatives
+    The Homefragment still uses savedInstanceState and clickListeners with DataBinding
+     */
+
+    lateinit var binding: FragmentHomeBinding
+    var numberOfOncreates = 0
+    var numberOfOncreateViews = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // If there isn't a bundle, then it's a "fresh" start
+        if (savedInstanceState != null) {
+            // Get all the game state information from the bundle, set it
+            numberOfOncreates = savedInstanceState.getInt("onCreates", 0)
+            numberOfOncreateViews = savedInstanceState.getInt("onCreateViews", 0)
+
+        }
+        numberOfOncreates+=1
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        //Step 1, use databinding to inflate the xml
+        numberOfOncreateViews+=1
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        setOnClickListeners()
+
+        setHasOptionsMenu(true)
+
+        Toast.makeText(context, "OnCreate: ${numberOfOncreates} - OCView: ${numberOfOncreateViews}", Toast.LENGTH_SHORT).show()
+
+        return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+    private fun setOnClickListeners() {
+        /*
+        binding.startjokesButton.setOnClickListener {
+            //Toast.makeText(activity, "Let's navigate!", Toast.LENGTH_SHORT).show()
+            view -> view.findNavController().navigate(R.id.action_homeFragment2_to_jokeFragment)
+         }
+         */
+
+        //alternatively
+//        binding.startjokesButton.setOnClickListener (
+//            Navigation.createNavigateOnClickListener(R.id.action_homeFragment2_to_jokeFragment)
+//        )
+//        binding.toOverview.setOnClickListener(
+//            Navigation.createNavigateOnClickListener(R.id.action_homeFragment2_to_jokeOverviewFragment)
+//        )
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("onCreateViews", numberOfOncreateViews)
+        outState.putInt("onCreates", numberOfOncreates)
+        super.onSaveInstanceState(outState)
+    }
 }
