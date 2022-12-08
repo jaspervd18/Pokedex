613e6900e427bbe47a0fb18de6069bafebbe7427
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-08 19:17:47 +0100

    finished favoriteFragment

--- app/src/main/java/com/example/pokedex/screens/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/pokedex/screens/favorites/FavoritesFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.pokedex.R
@@ -32,7 +34,16 @@ class FavoritesFragment : Fragment() {
         binding.favoritesViewModel = viewModel
         binding.lifecycleOwner = this
 
-        val adapter =
+        // filling the list: favorites adapter
+        val adapter = FavoritesAdapter(FavoritesListener { jokeID ->
+            Toast.makeText(context, "${jokeID}", Toast.LENGTH_SHORT).show()
+        })
+        binding.favoritesList.adapter = adapter
+
+        // watch the data:
+        viewModel.favorites.observe(viewLifecycleOwner, Observer {
+            adapter.submitList(it)
+        })
 
         return binding.root
     }
