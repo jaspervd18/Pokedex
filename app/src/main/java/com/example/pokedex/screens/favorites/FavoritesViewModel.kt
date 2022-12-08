d8ec07ad5580842d2c16074b67a64d79a64b9ca7
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-08 20:19:16 +0100

    clear all favorites button + implemented

--- app/src/main/java/com/example/pokedex/screens/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/example/pokedex/screens/favorites/FavoritesViewModel.kt
@@ -1,10 +1,29 @@
 package com.example.pokedex.screens.favorites
 
 import android.app.Application
+import androidx.constraintlayout.widget.ConstraintSet.Transform
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.viewModelScope
 import com.example.pokedex.database.favorites.FavoriteDatabaseDao
+import kotlinx.coroutines.launch
 
 class FavoritesViewModel(val database: FavoriteDatabaseDao, application: Application) :
     AndroidViewModel(application) {
     val favorites = database.getAllFavorites()
+
+    val clearButtonVisible = Transformations.map(favorites) {
+        it?.isNotEmpty()
+    }
+
+    private suspend fun clear() {
+        database.clear()
+    }
+
+    fun onClear() {
+        viewModelScope.launch {
+            // Clear the database table.
+            clear()
+        }
+    }
 }
