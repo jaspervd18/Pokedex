df90c0f60edc6a15be907bbdd82cae14f92b7ca3
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-08 17:40:40 +0100

    FavoriteDatabase

---
+++ app/src/main/java/com/example/pokedex/database/favorites/FavoriteDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.pokedex.database.favorites
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [DatabaseFavorite::class], version = 1, exportSchema = false)
+abstract class FavoriteDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: FavoriteDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: FavoriteDatabase? = null
+
+        fun getInstance(context: Context): FavoriteDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        FavoriteDatabase::class.java,
+                        "sleep_history_database"
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
+
