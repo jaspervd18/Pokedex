84f58f894e324b20da157f6c4f2ec78ff418a9a9
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-08 15:49:43 +0100

    add favorite button to pokemonFragment

--- app/src/main/java/com/example/pokedex/screens/pokemons/PokemonFragment.kt
+++ app/src/main/java/com/example/pokedex/screens/pokemons/PokemonFragment.kt
@@ -1,18 +1,14 @@
 package com.example.pokedex.screens.pokemons
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.pokedex.R
 import com.example.pokedex.databinding.FragmentPokemonBinding
-import com.example.pokedex.domain.Pokemon
 
 class PokemonFragment : Fragment() {
 
@@ -27,19 +23,10 @@ class PokemonFragment : Fragment() {
 
         // ViewModel and ViewModelFactory
         val viewModelFactory = PokemonViewModelFactory()
-//        viewModel = ViewModelProviders.of(this, viewModelFactory).get(PokemonViewModel::class.java)
         val viewModel : PokemonViewModel by viewModels{viewModelFactory}
 
         binding.pokemonViewModel = viewModel
         binding.setLifecycleOwner (this)
-
-        // Id and Name Observers
-//        viewModel.pokemonNr.observe(viewLifecycleOwner, Observer { newId ->
-//            binding.pokemonId.text = newId.toString()
-//        })
-//        viewModel.name.observe(viewLifecycleOwner, Observer { newName ->
-//            binding.pokemonName.text = newName
-//        })
 
         return binding.root
     }
