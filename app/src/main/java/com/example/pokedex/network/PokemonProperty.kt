ed53190d264bab0ce3a52407266341ce9a896a5b
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-21 16:08:24 +0100

    created pokemonProperty

--- app/src/main/java/com/example/pokedex/network/PokemonProperty.kt
+++ app/src/main/java/com/example/pokedex/network/PokemonProperty.kt
@@ -1,11 +1,24 @@
 package com.example.pokedex.network
 
 import com.example.pokedex.domain.Pokemon
+import com.squareup.moshi.Json
 
-data class ApiPokemon(
-    val body: List<Pokemon>
-) {
-    //Hardcoded image url to demo Glide
-    val squirtleUri =
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
-}
+data class PokemonProperty(
+    val id: Int,
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    val sprites: Sprites
+)
+
+data class Sprites(
+    @field:Json(name = "other") val otherSprites: List<OtherSprites>
+)
+
+data class OtherSprites(
+    @field:Json(name = "official-artwork") val officialArtwork: OfficalArtwork
+)
+
+data class OfficalArtwork(
+    @Json(name="front_default") val frontDefault: String
+)
