b3018b62bdd7deb8eafb94d0e15d27f20dc78af0
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-24 15:37:53 +0100

    created repository

---
+++ app/src/main/java/com/example/pokedex/repository/FavoritePokemonRepository.kt
@@ -0,0 +1,15 @@
+package com.example.pokedex.repository
+
+import com.example.pokedex.database.favorites.FavoriteDatabase
+import com.example.pokedex.network.PokemonApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class FavoritePokemonRepository(private val database: FavoriteDatabase) {
+
+//    suspend fun refreshFavoritePokemon() {
+//        withContext(Dispatchers.IO) {
+//            val favoritePokemon = PokemonApi.retrofitService.getPokemon(1)
+//        }
+//    }
+}
