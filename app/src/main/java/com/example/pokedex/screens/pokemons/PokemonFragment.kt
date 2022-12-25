3b7b09822786a8c55a480a5b3d7004803326c015
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-25 17:04:39 +0100

    optimized Pokemon fragment class

--- app/src/main/java/com/example/pokedex/screens/pokemons/PokemonFragment.kt
+++ app/src/main/java/com/example/pokedex/screens/pokemons/PokemonFragment.kt
@@ -1,12 +1,11 @@
 package com.example.pokedex.screens.pokemons
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.pokedex.R
 import com.example.pokedex.database.favorites.FavoriteDatabase
@@ -15,12 +14,12 @@ import com.example.pokedex.databinding.FragmentPokemonBinding
 class PokemonFragment : Fragment() {
 
     lateinit var binding: FragmentPokemonBinding
-    lateinit var viewModel: PokemonViewModel
+    private lateinit var viewModel: PokemonViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Binding
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pokemon, container, false)
 
@@ -30,19 +29,17 @@ class PokemonFragment : Fragment() {
 
         // ViewModel and ViewModelFactory
         val viewModelFactory = PokemonViewModelFactory(dataSource, appContext)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(PokemonViewModel::class.java)
+        viewModel = ViewModelProvider(this, viewModelFactory)[PokemonViewModel::class.java]
 
         binding.pokemonViewModel = viewModel
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this
 
-        viewModel.saveEvent.observe(viewLifecycleOwner, Observer { saveEvent ->
+        viewModel.saveEvent.observe(viewLifecycleOwner) { saveEvent ->
             if (saveEvent) {
                 viewModel.favoritePokemon()
                 viewModel.saveEventDone()
             }
-        })
-
-
+        }
 
         return binding.root
     }
