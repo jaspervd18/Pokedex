628611e56ad2f7d94737459aff65d594a7ad700d
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-25 16:32:27 +0100

    use dao instead of database

--- app/src/main/java/com/example/pokedex/repository/FavoritePokemonRepository.kt
+++ app/src/main/java/com/example/pokedex/repository/FavoritePokemonRepository.kt
@@ -2,8 +2,7 @@ package com.example.pokedex.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
-import com.example.pokedex.database.favorites.DatabaseFavorite
-import com.example.pokedex.database.favorites.FavoriteDatabase
+import com.example.pokedex.database.favorites.FavoriteDatabaseDao
 import com.example.pokedex.database.favorites.asDomainModel
 import com.example.pokedex.domain.Pokemon
 import com.example.pokedex.network.PokemonApi
@@ -11,10 +10,10 @@ import com.example.pokedex.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class FavoritePokemonRepository(private val database: FavoriteDatabase) {
+class FavoritePokemonRepository(private val database: FavoriteDatabaseDao) {
 
     val favoritePokemons: LiveData<List<Pokemon>> =
-        Transformations.map(database.favoriteDatabaseDao.getAllFavorites()) {
+        Transformations.map(database.getAllFavorites()) {
             it.asDomainModel()
         }
 
@@ -23,7 +22,7 @@ class FavoritePokemonRepository(private val database: FavoriteDatabase) {
             for (favorite in favoritePokemons.value!!) {
                 val favoritePokemon =
                     PokemonApi.retrofitService.getPokemonAsync(favorite.pokemonNr).await()
-                database.favoriteDatabaseDao.insert(favoritePokemon.asDatabaseModel())
+                database.insert(favoritePokemon.asDatabaseModel())
             }
         }
     }
