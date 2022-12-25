5d41efcb8f1720ea9c9e237064c8653130198a75
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-25 15:48:13 +0100

    defined the main interface for favoritePokemonRepository

--- app/src/main/java/com/example/pokedex/repository/FavoritePokemonRepository.kt
+++ app/src/main/java/com/example/pokedex/repository/FavoritePokemonRepository.kt
@@ -1,7 +1,11 @@
 package com.example.pokedex.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import com.example.pokedex.database.favorites.DatabaseFavorite
 import com.example.pokedex.database.favorites.FavoriteDatabase
+import com.example.pokedex.database.favorites.asDomainModel
+import com.example.pokedex.domain.Pokemon
 import com.example.pokedex.network.PokemonApi
 import com.example.pokedex.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
@@ -9,17 +13,17 @@ import kotlinx.coroutines.withContext
 
 class FavoritePokemonRepository(private val database: FavoriteDatabase) {
 
+    val favoritePokemons: LiveData<List<Pokemon>> =
+        Transformations.map(database.favoriteDatabaseDao.getAllFavorites()) {
+            it.asDomainModel()
+        }
+
     suspend fun refreshFavoritePokemon() {
         withContext(Dispatchers.IO) {
-            val favoritePokemons: List<DatabaseFavorite>? =
-                database.favoriteDatabaseDao.getAllFavorites().value
-
-            if (favoritePokemons != null) {
-                for (favorite in favoritePokemons) {
-                    val favoritePokemon =
-                        PokemonApi.retrofitService.getPokemonAsync(favorite.pokemonNr).await()
-                    database.favoriteDatabaseDao.insert(favoritePokemon.asDatabaseModel())
-                }
+            for (favorite in favoritePokemons.value!!) {
+                val favoritePokemon =
+                    PokemonApi.retrofitService.getPokemonAsync(favorite.pokemonNr).await()
+                database.favoriteDatabaseDao.insert(favoritePokemon.asDatabaseModel())
             }
         }
     }
