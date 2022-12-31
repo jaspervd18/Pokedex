40e4c31053cae74c8ef6a4bee70428292f767846
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-31 12:04:21 +0100

    documented FavoritesFragment

--- app/src/main/java/com/example/pokedex/screens/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/pokedex/screens/favorites/FavoritesFragment.kt
@@ -7,50 +7,61 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.pokedex.R
 import com.example.pokedex.database.favorites.FavoriteDatabase
 import com.example.pokedex.databinding.FragmentFavoritesBinding
 
+/**
+ * This fragment shows the favorite pokemon saved in the database
+ */
 class FavoritesFragment : Fragment() {
 
-    lateinit var binding: FragmentFavoritesBinding
-    lateinit var viewModel: FavoritesViewModel
-    lateinit var adapter: FavoritesAdapter
+    /**
+     * Initialize Favoritebinding.
+     */
+    private lateinit var binding: FragmentFavoritesBinding
 
+    /**
+     * Initialize ViewModel.
+     */
+    private lateinit var viewModel: FavoritesViewModel
+    private lateinit var adapter: FavoritesAdapter
+
+    /**
+     * Inflates the layout with Data Binding, sets its lifecycle owner to the FavoritesFragment
+     * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
+     */
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorites, container, false)
 
+        // Get an instance of the appContext to setup the database
         val application = requireNotNull(this.activity).application
         val dataSource = FavoriteDatabase.getInstance(application).favoriteDatabaseDao
 
+        // ViewModel and ViewModelFactory
         val viewModelFactory = FavoritesViewModelFactory(dataSource, application)
         viewModel = ViewModelProvider(this, viewModelFactory)[FavoritesViewModel::class.java]
 
+        // Giving the binding access to the OverviewViewModel
         binding.favoritesViewModel = viewModel
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
 
-        // filling the list: favorites adapter
-        adapter = FavoritesAdapter(
-            FavoritesListener { pokemonName ->
-                Toast.makeText(context, "$pokemonName", Toast.LENGTH_SHORT).show()
-            }
-        )
+        // Filling the list with the favorites adapter
+        adapter = FavoritesAdapter(FavoritesListener { pokemonName ->
+            Toast.makeText(context, pokemonName, Toast.LENGTH_SHORT).show()
+        })
         binding.favoritesList.adapter = adapter
 
-        // watch the data:
-        viewModel.favorites.observe(
-            viewLifecycleOwner,
-            Observer {
-                adapter.submitList(it)
-            }
-        )
+        // Observe the data
+        viewModel.favorites.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
 
         return binding.root
     }
