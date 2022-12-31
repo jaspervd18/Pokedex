d1e6ddd6322c3ba4a6caca25fbb077ce88746ee0
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-31 11:22:10 +0100

    pokemon class documentation

--- app/src/main/java/com/example/pokedex/network/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/network/Pokemon.kt
@@ -1,15 +1,13 @@
 package com.example.pokedex.network
 
-import androidx.lifecycle.LiveData
 import com.example.pokedex.database.favorites.DatabaseFavorite
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 /**
  * Gets Pokemon information from the Pokemon API Retrofit service and updates the
- * [Pokemon] and [PokemonkApiStatus] [LiveData]. The Retrofit service returns a coroutine
+ * [Pokemon]. The Retrofit service returns a coroutine
  * Deferred, which we await to get the result of the transaction.
- * @param filter the [MarsApiFilter] that is sent as part of the web server request
  */
 @JsonClass(generateAdapter = true)
 data class Pokemon(
@@ -21,28 +19,42 @@ data class Pokemon(
     val types: List<Types>
 )
 
+
+data class Types(
+    val slot: Int, val type: Type
+)
+
+data class Type(
+    val name: String, val url: String
+)
+
+/**
+ * Gets Sprites information from the Pokemon.
+ * Multiple sprites exist, here only other sprites are asked.
+ */
 data class Sprites(
     val other: Others
 )
 
-data class Types(
-    val slot: Int,
-    val type: Type
-)
-
+/**
+ *  The other sprites contains the official artwork from the Pokemon.
+ *  This artwork will be used throughout the whole app.
+ */
 data class Others(
     @Json(name = "official-artwork") val officialArtwork: OfficialArtwork
 )
 
-data class Type(
-    val name: String,
-    val url: String
-)
-
+/**
+ * Official Artwork contains the String with the image URL
+ */
 data class OfficialArtwork(
     @Json(name = "front_default") val frontDefault: String
 )
 
+/**
+ * Convert network result into Database Favorite Pokemon
+ * Returns a Database Favorite
+ **/
 fun Pokemon.asDatabaseModel(): DatabaseFavorite {
     return DatabaseFavorite(
         pokemonNr = id,
