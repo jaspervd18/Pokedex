0035e87735ea696de26211aa482d1a84913c6229
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-12-31 11:40:25 +0100

    cleaned up HomeFragment

--- app/src/main/java/com/example/pokedex/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/pokedex/screens/home/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.example.pokedex.screens.home
 
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -18,72 +17,39 @@ import com.example.pokedex.databinding.FragmentHomeBinding
  */
 class HomeFragment : Fragment() {
 
-    /*
-    Note: the HomeFragment doesn't follow the MVVM architecture to demonstrate the alternatives
-    The Homefragment still uses savedInstanceState and clickListeners with DataBinding
-     */
-
     lateinit var binding: FragmentHomeBinding
-    var numberOfOncreates = 0
-    var numberOfOncreateViews = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // If there isn't a bundle, then it's a "fresh" start
-        if (savedInstanceState != null) {
-            // Get all the game state information from the bundle, set it
-            numberOfOncreates = savedInstanceState.getInt("onCreates", 0)
-            numberOfOncreateViews = savedInstanceState.getInt("onCreateViews", 0)
-        }
-        numberOfOncreates += 1
-    }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        // Step 1, use databinding to inflate the xml
-        numberOfOncreateViews += 1
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
         setOnClickListeners()
 
         setHasOptionsMenu(true)
 
-        Toast.makeText(
-            context,
-            "OnCreate: $numberOfOncreates - OCView: $numberOfOncreateViews",
-            Toast.LENGTH_SHORT
-        ).show()
-
         return binding.root
     }
 
+    /**
+     * Inflates the overflow menu.
+     */
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
+    /**
+     * Navigates to selected component
+     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return NavigationUI.onNavDestinationSelected(
-            item!!,
-            requireView().findNavController()
-        ) ||
-            super.onOptionsItemSelected(item)
+            item!!, requireView().findNavController()
+        ) || super.onOptionsItemSelected(item)
     }
 
     private fun setOnClickListeners() {
-        /*
-        binding.startjokesButton.setOnClickListener {
-            //Toast.makeText(activity, "Let's navigate!", Toast.LENGTH_SHORT).show()
-            view -> view.findNavController().navigate(R.id.action_homeFragment2_to_jokeFragment)
-         }
-         */
-
-        // alternatively
         binding.gotopokedexButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_pokemonFragment)
         )
@@ -92,9 +58,4 @@ class HomeFragment : Fragment() {
         )
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt("onCreateViews", numberOfOncreateViews)
-        outState.putInt("onCreates", numberOfOncreates)
-        super.onSaveInstanceState(outState)
-    }
 }
